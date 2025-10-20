a6423082eacc7b473d3be3e865260445269197f3
Levi K <levi.knoll2022@gmail.com>
2025-10-20 13:21:12 -0700

    Update social card and logo image paths

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/HoneyMesh_PictureLogo.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
@@ -86,7 +86,7 @@ const config = {
         title: 'HoneyMesh',
         logo: {
           alt: 'HoneyMesh logo',
-          src: 'img/logo.svg',
+          src: 'img/HoneyMesh_PictureLogo.png',
         },
         
       },
