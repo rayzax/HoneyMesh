fa48d92649efcee7cc32deb25e59149f921c9c92
Andrew Syverson <rayzaxs@gmail.com>
2025-10-26 12:20:11 -0700

    Medium Interaction Feature

---
+++ medium/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+HoneyMesh Medium Interaction Module
+Integration layer for custom honeypot deployment
+"""
+
+from .medium_deployment import MediumDeploymentManager
+
+__all__ = ['MediumDeploymentManager']
+__version__ = '1.0.0'
