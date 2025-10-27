61c639971c86e073f36e1304b7e70109acb9eb7b
Levi K <levi.knoll2022@gmail.com>
2025-10-27 13:39:43 -0700

    Update image for 'Easy to Deploy' feature

--- docs/src/components/HomepageFeatures/index.js
+++ docs/src/components/HomepageFeatures/index.js
@@ -7,7 +7,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Easy to Deploy',
-    img: require('@site/static/img/EasyToDeploy_HoneyMesh.png').default,
+    img: require('@site/static/img/EasyToDeploy_HoneyMesh3.png').default,
     description: (
       <>
         HoneyMesh is built from the ground up to make honeypot deployment simple
